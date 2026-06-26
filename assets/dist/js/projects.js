c25705a23525b53bffc55207b75c0d81e382ada7
tapia41 <tapia41@gmail.com>
2026-06-26 13:28:44 -0600

    updated

--- assets/dist/js/projects.js
+++
@@ -1,4 +0,0 @@
-var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
-    interval: 2000, // Set the interval between slide transitions (in milliseconds)
-    pause: 'hover', // Pause on hover
-});
